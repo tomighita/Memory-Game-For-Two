b20cfc4de5ed9352f43f7bc9ae83f67567ca3d4d
Victor Ghita <tomi.gvf@gmail.com>
2022-01-11 20:15:40 +0100

    Blob commit

--- MyProject/routes/index.js
+++ MyProject/routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', (req,res,next) => {
+  res.sendFile("splash.html", {root: "./public"}, (err) => {
+    console.error(err);
+  });
+});
+
+router.get('/game', (req,res,next) => {
+  res.sendFile("game.html", {root: "./public"}, (err) => {
+    console.error(err);
+  });
 });
 
 module.exports = router;
